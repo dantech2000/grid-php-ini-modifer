6e6062a9e82803309e2336f43d26c86aeac7d5e6
Daniel Rodriguez <dantech2000@gmail.com>
2016-11-23 02:30:47 -0800

    Added some additional functions and global variables

--- grid-php-ini-modifer.php
+++ grid-php-ini-modifer.php
@@ -12,60 +12,61 @@
 * Copyright 2014-2015 (mt) Media Temple, Inc. All Rights Reserved.
 */
 
-global $grid_site_idenfier = getenv($SITE);
-global $path_to_ini = '\/home/' . $grid_site_idenfier . '\/etc/'
-
+// Setup global variables
+global $plugin_update_ver;
+$plugin_update_ver = '0.1'
+global $path_to_ini;
+$path_to_ini = '\/home/' . $grid_site_idenfier . '\/etc/'
+global $grid_site_idenfier;
+$grid_site_idenfier = getenv( $SITE );
 
 // Make sure it's wordpress
 if ( !defined( 'ABSPATH' ) ) {
     die( 'Forbidden' );
 }
 
-// Create .user.ini
-function gpim_Create_File(){
+// Create .user.ini if does not exist
+function gpim_Create_User_ini(){
   $create_user_ini = fopen(".user.ini", "r");
   // some code to be executed....
-  fclose($create_user_ini);
+  fclose( $create_user_ini );
 }
 
 // Create php.ini if does not exist
-function gpim_Create_File(){
-  string getenv ( string $varname )
-  $create_php_ini = fopen("php.ini", "r");
+function gpim_Create_Php_ini(){
+  $create_php_ini = fopen( "php.ini", "r" );
   // some code to be executed....
-  fclose($create_php_ini;
+  fclose( $create_php_ini );
 }
 
-
-
-
-// Hook into Admin menu load
-add_action('admin_menu', 'gpim_setup_menu');
 
 // (Title of the menu, Label for admin panel, user group with access, function to build html page)
 function gpim_setup_menu(){
   add_menu_page( 'Grid PHP INI Modifer', 'Grid PHP INI Modifer', 'manage_options', 'gpim-plugin', 'gpim_init' );
 }
+// Hook into Admin menu load
+add_action( 'admin_menu', 'gpim_setup_menu' );
 
-
-// Function that loads the admin menu for GPIM
+// This function loads the admin page for GPIM
 function gpim_init(){
   // PHP.ini file path
   $inipath = php_ini_loaded_file();
   // Check if ini exist, if show loaded from + path
-  if($inipath){
+  if( $inipath ){
     echo 'PHP.ini loaded from: ' . $inipath;
     echo $path_to_ini
+    echo $grid_site_idenfier
   } else {
     echo "ERROR: Could not find php.ini";
   }
 
 }
+// Hook into Admin menu load
+add_action( 'admin_init', 'gpim_init' );
 
-function gpim_Create_File(){
-  $myfile = fopen(".user.ini", "r");
-  // some code to be executed....
-  fclose($myfile);
+function gpim_self_upgrade() {
+    //some code to be executed
 }
+add_action( 'admin_init', 'gpim_self_upgrade' );
 
 ?>
